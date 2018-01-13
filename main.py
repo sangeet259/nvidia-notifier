c58fc990ff3dfcee40ce98e773ee337f1b581603
sangeet259 <mail2sangeetmishra@gmail.com>
2018-01-13 14:20:48 +0530

    get nvidia-smi output

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import subprocess
+import subprocess as sp
 
 # Search for the part we want to get via regex
 reg=r"\d+[MiB]+....\d+[MiB]+"
+
+# get nvidia-smi output
+
+result=sp.getoutput("nvidia-smi")
+
