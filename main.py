fec5138cbf9ab838534b1bc26293b48b006f25de
sangeet259 <mail2sangeetmishra@gmail.com>
2018-01-13 14:42:16 +0530

    implements checking over fixed time periods

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import subprocess as sp
 import re
+import time
 
 # Search for the part we want to get via regex
 reg=r"\d+[MiB]+....\d+[MiB]+"
 
-# get nvidia-smi output
 
-result=sp.getoutput("nvidia-smi")
 
-matches = re.findall(reg, result)
+check_time_period=60
 
-sp.call(['notify-send','GPU-Usage-Status',matches[0]])
+
+while True:
+	# get nvidia-smi output
+	result=sp.getoutput("nvidia-smi")
+
+	matches = re.findall(reg, result)
+	sp.call(['notify-send','GPU-Usage-Status',matches[0]])
+	time.sleep(check_time_period)
